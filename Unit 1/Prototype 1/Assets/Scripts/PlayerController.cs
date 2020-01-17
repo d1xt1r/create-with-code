8ff94e42b86b594dc2e8672112d7461fe166deb8
d1xt1r <d1xt1r@gmail.com>
2020-01-17 13:27:17 +0200

    Scene changes

--- Unit 1/Prototype 1/Assets/Scripts/PlayerController.cs
+++ Unit 1/Prototype 1/Assets/Scripts/PlayerController.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
-    private float speed = 5.0f;
-    private float turnSpeed = 25.0f;
+    private float speed = 20.0f;
+    private float turnSpeed = 80.0f;
     private float horizontalInput;
     private float verticalInput;
 
