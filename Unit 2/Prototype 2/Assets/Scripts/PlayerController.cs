987d4dc0706f0e60030616a8c4dacc83bf665e4a
d1xt1r <d1xt1r@gmail.com>
2020-01-23 10:15:06 +0200

    Adding some coments in the code

--- Unit 2/Prototype 2/Assets/Scripts/PlayerController.cs
+++ Unit 2/Prototype 2/Assets/Scripts/PlayerController.cs
@@ -17,16 +17,20 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        // Limit player from going offscreen in left direction
         if (transform.position.x < -xRange) 
         {
             transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
         }
-
+        // Limit player from going offscreen in right direction
         if (transform.position.x > xRange) {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
 
+        // Get horizontal input (either W,S or <-.->) from the keyboard
         horizontalInput = Input.GetAxis("Horizontal");
+        // Making the player move by multipying the necessary things
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
     }
 }
