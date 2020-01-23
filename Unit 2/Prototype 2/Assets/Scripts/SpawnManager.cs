e0cb48eda260340a5df1da3622eb006dfcc610a1
d1xt1r <d1xt1r@gmail.com>
2020-01-23 14:57:33 +0200

    SpawnManager script modified in order to spawn random enemies when S is pressed

--- Unit 2/Prototype 2/Assets/Scripts/SpawnManager.cs
+++ Unit 2/Prototype 2/Assets/Scripts/SpawnManager.cs
@@ -4,9 +4,7 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-
     public GameObject[] animalPrefabs; // Creating an array of prefabs
-    public int animalIndex; // Index of the array
 
     // Start is called before the first frame update
     void Start()
@@ -17,8 +15,10 @@ public class SpawnManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.S)) // When S is pressed, instanitiate an enemy from the array at this position at the same rotation.
+        if (Input.GetKeyDown(KeyCode.S)) // When S is pressed ->
         {
+            int animalIndex = Random.Range(0, animalPrefabs.Length); // Index of the array is set to random from 0 to 3 (3 is exclusive);
+            // -> instanitiate an random enemy from the array at z position and do not change the rotation
             Instantiate(animalPrefabs[animalIndex], new Vector3(0, 0, 20), animalPrefabs[animalIndex].transform.rotation);
         }
     }
