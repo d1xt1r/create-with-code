324098b8dca1287d5d79af2e766106b4f2a32355
d1xt1r <d1xt1r@gmail.com>
2020-01-23 12:50:04 +0200

    Add logic to destory enemies offscreen

--- Unit 2/Prototype 2/Assets/Scripts/DestroyOutOfBounds.cs
+++ Unit 2/Prototype 2/Assets/Scripts/DestroyOutOfBounds.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class DestroyOutOfBounds : MonoBehaviour
 {
     private float topBound = 30.0f;
+    private float lowerBound = -10.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,8 +15,11 @@ public class DestroyOutOfBounds : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // When projectile flies off the top boundry it will destroy itself.
-        if (transform.position.z > topBound)
+        
+        if (transform.position.z > topBound) // When projectile flies off the top boundry it will destroy itself.
+        {
+            Destroy(gameObject);
+        } else if (transform.position.z < lowerBound) // When enemies flies off the lower boundry it will destroy itself.
         {
             Destroy(gameObject);
         }
