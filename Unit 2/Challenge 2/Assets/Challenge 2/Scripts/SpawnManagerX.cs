8161a064bb52f2b634a72610c0253108c578c7c6
d1xt1r <d1xt1r@gmail.com>
2020-01-24 17:44:41 +0200

    Create new int variable to store a random index from the array

--- Unit 2/Challenge 2/Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ Unit 2/Challenge 2/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -26,7 +26,8 @@ public class SpawnManagerX : MonoBehaviour
         Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
 
         // instantiate ball at random spawn location
-        Instantiate(ballPrefabs[0], spawnPos, ballPrefabs[0].transform.rotation);
+        int ballIndex = Random.Range(0, ballPrefabs.Length); // Store a random number from the index as integer, so I can use it in the Instantiate method.
+        Instantiate(ballPrefabs[ballIndex], spawnPos, ballPrefabs[0].transform.rotation);
     }
 
 }
