534d073fe04a8b31fb56bd8ccbcc191f79f170bd
d1xt1r <d1xt1r@gmail.com>
2020-01-26 12:56:17 +0200

    SpawnManager script created for spawning obstacles at th scene

---
+++ Unit 3/Prototype 3/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject obstaclePrefab; // Drag and drop the prefab of the obstacle
+    private Vector3 spawnPos = new Vector3(25, 0, 0); // Position at we want our obstacles to spawn
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation); // Spawn a copy of the prefab at the spawnPos and no rotation (the default on the prefab)
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
