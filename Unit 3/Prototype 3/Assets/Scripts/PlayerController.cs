b6d2a9b4f699ef8d90f10a97ebd61231bf5759b3
d1xt1r <d1xt1r@gmail.com>
2020-01-26 12:06:35 +0200

    Prevent player from double-jumping

--- Unit 3/Prototype 3/Assets/Scripts/PlayerController.cs
+++ Unit 3/Prototype 3/Assets/Scripts/PlayerController.cs
@@ -4,24 +4,32 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-
     private Rigidbody playerRb;
     public float jumpForce = 10;
     public float gravityModifier = 2;
+    public bool isOnGround = true;
 
     // Start is called before the first frame update
     void Start()
     {
-        playerRb = GetComponent<Rigidbody>();
-        Physics.gravity *= gravityModifier;
+        playerRb = GetComponent<Rigidbody>(); // Get the Rigidbody component of the player
+        Physics.gravity *= gravityModifier; // Modifying the default gravity so jumps can feel more natural
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround) // You can jump by pressing space when the player is on the ground
         {
-            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse); // Applying jumpforce to the player rb so he can jump
+            isOnGround = false; // When you are in the air you cannot jump again
         }
+    }
+
+    // Reset isOnGround to true, when the player touches the groud, so you can jump again
+    private void OnCollisionEnter(Collision collision)
+    {
+        isOnGround = true;
     }
 }
