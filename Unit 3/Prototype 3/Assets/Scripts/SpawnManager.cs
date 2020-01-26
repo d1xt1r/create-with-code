4a63cb917547ad886759e5cf222773d46df98af2
d1xt1r <d1xt1r@gmail.com>
2020-01-26 13:09:56 +0200

    Custom SpawnObstacle method created

--- Unit 3/Prototype 3/Assets/Scripts/SpawnManager.cs
+++ Unit 3/Prototype 3/Assets/Scripts/SpawnManager.cs
@@ -6,16 +6,23 @@ public class SpawnManager : MonoBehaviour
 {
     public GameObject obstaclePrefab; // Drag and drop the prefab of the obstacle
     private Vector3 spawnPos = new Vector3(25, 0, 0); // Position at we want our obstacles to spawn
+    private float startDelay = 2.0f;
+    private float repeatRate = 2.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation); // Spawn a copy of the prefab at the spawnPos and no rotation (the default on the prefab)
+        InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void SpawnObstacle()
+    {
+        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation); // Spawn a copy of the prefab at the spawnPos and no rotation (the default on the prefab)
     }
 }
