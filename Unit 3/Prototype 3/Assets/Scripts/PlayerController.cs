f28aad5753d59d0ed0638b3f179e2daa82e2897b
d1xt1r <d1xt1r@gmail.com>
2020-01-26 11:40:31 +0200

    Make player jump if spacebar pressed

--- Unit 3/Prototype 3/Assets/Scripts/PlayerController.cs
+++ Unit 3/Prototype 3/Assets/Scripts/PlayerController.cs
@@ -11,12 +11,14 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
-        playerRb.AddForce(Vector3.up * 500);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            playerRb.AddForce(Vector3.up * 10, ForceMode.Impulse);
+        }
     }
 }
