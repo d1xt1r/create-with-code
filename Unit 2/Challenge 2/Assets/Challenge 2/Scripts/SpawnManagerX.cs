729c2caa6adf82a10a64200142e792a0f90d6042
d1xt1r <d1xt1r@gmail.com>
2020-01-26 17:26:43 +0200

    SpawnManagerX code refactoring for Challenge 2

--- Unit 2/Challenge 2/Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ Unit 2/Challenge 2/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -9,34 +9,23 @@ public class SpawnManagerX : MonoBehaviour
     private float spawnLimitXLeft = -22;
     private float spawnLimitXRight = 7;
     private float spawnPosY = 30;
-
     private float startDelay;
-    //private float spawnInterval;
 
     // Start is called before the first frame update
     void Start()
     {
-        // Question 7 - Make the spawn interval a random value between 3 seconds and 5 seconds
-        Invoke("SpawnRandomBall", startDelay);
-
-        //InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
+        Invoke("SpawnRandomBall", startDelay); // Question 7 - Make the spawn interval a random value between 3 seconds and 5 seconds
     }
 
     // Spawn random ball at random x position at top of play area
     void SpawnRandomBall ()
     {
-        // Question 6 - Generate a random number  from the index, so you can spawn all types of balls
-        int ballIndex = Random.Range(0, ballPrefabs.Length);
-
-        // Generate random ball index and random spawn position
-        Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
-
-       // Instantiate ball at random spawn location
-        Instantiate(ballPrefabs[ballIndex], spawnPos, ballPrefabs[0].transform.rotation);
+        int ballIndex = Random.Range(0, ballPrefabs.Length); // Question 6 - Generate a random number  from the index, so you can spawn all types of balls
+        Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0); // Generate random ball index and random spawn position
+        Instantiate(ballPrefabs[ballIndex], spawnPos, ballPrefabs[0].transform.rotation); // Instantiate ball at random spawn location
 
         // Question 7 - Make the spawn interval a random value between 3 seconds and 5 seconds
         startDelay = Random.Range(1, 4);
         Invoke("SpawnRandomBall", startDelay);
     }
-
 }
