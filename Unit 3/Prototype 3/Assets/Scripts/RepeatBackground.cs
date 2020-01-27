47aa1235366aebceb9f4116ad3d63ce9d829151c
d1xt1r <d1xt1r@gmail.com>
2020-01-27 14:37:03 +0200

    Reset position of background

--- Unit 3/Prototype 3/Assets/Scripts/RepeatBackground.cs
+++ Unit 3/Prototype 3/Assets/Scripts/RepeatBackground.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class RepeatBackground : MonoBehaviour
 {
+    private Vector3 startPos; // Variable for storing the starting position of the Background
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        startPos = transform.position; // Starting position of the Background
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (transform.position.x < startPos.x - 70) // If the current position of the Background is less than the starting with an offset, reset the position
+        {
+            transform.position = startPos;
+        }
     }
 }
