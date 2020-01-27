758d04cbf57edd1096528f19477f35ca37658127
d1xt1r <d1xt1r@gmail.com>
2020-01-27 14:48:58 +0200

    Make the background transition smooth using a box collider and its size

--- Unit 3/Prototype 3/Assets/Scripts/RepeatBackground.cs
+++ Unit 3/Prototype 3/Assets/Scripts/RepeatBackground.cs
@@ -5,17 +5,19 @@ using UnityEngine;
 public class RepeatBackground : MonoBehaviour
 {
     private Vector3 startPos; // Variable for storing the starting position of the Background
+    private float repeatWidth;
 
     // Start is called before the first frame update
     void Start()
     {
         startPos = transform.position; // Starting position of the Background
+        repeatWidth = GetComponent<BoxCollider>().size.x / 2;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.x < startPos.x - 70) // If the current position of the Background is less than the starting with an offset, reset the position
+        if (transform.position.x < startPos.x - repeatWidth) // If the current position of the Background is less than the starting with an offset, reset the position
         {
             transform.position = startPos;
         }
