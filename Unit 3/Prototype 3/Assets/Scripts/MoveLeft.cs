668a09b16755dbf2a00951eff6fde5f354e97c20
d1xt1r <d1xt1r@gmail.com>
2020-01-28 16:40:10 +0200

    Stop MoveLeft on gameOver

--- Unit 3/Prototype 3/Assets/Scripts/MoveLeft.cs
+++ Unit 3/Prototype 3/Assets/Scripts/MoveLeft.cs
@@ -5,16 +5,25 @@ using UnityEngine;
 public class MoveLeft : MonoBehaviour
 {
     private float speed = 30;
+    private PlayerController PlayerControllerScript; // Variable for storing the reference to the actual PlayerController script
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        PlayerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>(); // Set the reference variable to the actual PlayerController script on the Player object.
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.left * Time.deltaTime * speed); // Moves the obsticle in left direction   
+        if (PlayerControllerScript.gameOver == false) // If gameOver (from the PlayerController script) condition is false continue move the obstacles in the left direction
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * speed); // Moves the obsticle in left direction 
+        }
+        else // if gameOver (from the PlayerController script) condition is true ->
+        {
+            transform.Translate(Vector3.zero); // -> Stop moving the obsticle in left direction  
+        }
     }
 }
